9724017270b19f6cfc0ff512f667fd17d30b19c3
Fateh Benmerzoug <fatehmtd@mail.com>
2021-07-11 18:17:17 -0400

    Enhanced minimap color rendering

--- RequestFlowApp/CoreView/MiniMap.cpp
+++ RequestFlowApp/CoreView/MiniMap.cpp
@@ -10,42 +10,52 @@ view::MiniMap::MiniMap()
     setFlag(GraphicsItemFlag::ItemIsSelectable);
 }
 
-#include <QPen>
 #include <QBrush>
-#include <QPainterPath>
 #include <QPainter>
+#include <QPainterPath>
+#include <QPen>
 
-#include <QDebug>
 #include "view/Colors.h"
+#include "view/Node.h"
 #include "view/SceneGraphWidget.h"
 #include <QDebug>
-#include "view/Node.h"
 
-void view::MiniMap::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+void view::MiniMap::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
 {
     auto rect = boundingRect();
+
+    // Outline
+    {
+        QPainterPath backgroundPath;
+        auto bgColor = colors::darkGrey;
+        QPen pen(bgColor, 1.0 * _invScale);
+
+        QBrush backgroundBrush(bgColor);
+        backgroundPath.addRect(0, 0, rect.width(), rect.height());
+        painter->setPen(pen);
+        painter->setBrush(QColor("#aaaaaa"));
+        painter->drawPath(backgroundPath);
+    }
+
     // Nodes
     {
         auto sceneRect = scene()->sceneRect();
 
         QList<Node*> nodes;
-        for(auto item : scene()->items())
-        {
+        for (auto item : scene()->items()) {
             auto node = dynamic_cast<view::Node*>(item);
-            if(node != nullptr)
-            {
+            if (node != nullptr) {
                 nodes << node;
             }
         }
 
-        for(auto node : nodes)
-        {
+        for (auto node : nodes) {
             auto p = node->scenePos();
-            float w = rect.width()*(node->width() / sceneRect.width());
-            float h = rect.height()*(node->height() / sceneRect.height());
+            float w = rect.width() * (node->width() / sceneRect.width());
+            float h = rect.height() * (node->height() / sceneRect.height());
 
-            float x = rect.width()*((p.x() - sceneRect.x()) / sceneRect.width());
-            float y = rect.height()*((p.y() - sceneRect.y()) / sceneRect.height());
+            float x = rect.width() * ((p.x() - sceneRect.x()) / sceneRect.width());
+            float y = rect.height() * ((p.y() - sceneRect.y()) / sceneRect.height());
 
             QPainterPath backgroundPath;
             auto bgColor = node->getBackgroundColor();
@@ -59,35 +69,22 @@ void view::MiniMap::paint(QPainter *painter, const QStyleOptionGraphicsItem *opt
         }
     }
 
-    // Outline
-    {
-        QPainterPath backgroundPath;
-        auto bgColor = colors::darkGrey;
-        QPen pen(bgColor);
-
-        QBrush backgroundBrush(bgColor);
-        backgroundPath.addRoundedRect(0, 0, rect.width(), rect.height(), 0, 0);
-        painter->setPen(pen);
-        painter->setBrush(Qt::NoBrush);
-        painter->drawPath(backgroundPath);
-    }
-
     // Viewport area
     {
         auto sceneRect = scene()->sceneRect();
 
-        float w = rect.width()*(_parentViewport.width() / sceneRect.width());
-        float h = rect.height()*(_parentViewport.height() / sceneRect.height());
+        float w = rect.width() * (_parentViewport.width() / sceneRect.width());
+        float h = rect.height() * (_parentViewport.height() / sceneRect.height());
 
-        float x = rect.width()*((_parentViewport.x() - sceneRect.x()) / sceneRect.width());
-        float y = rect.height()*((_parentViewport.y() - sceneRect.y()) / sceneRect.height());
+        float x = rect.width() * ((_parentViewport.x() - sceneRect.x()) / sceneRect.width());
+        float y = rect.height() * ((_parentViewport.y() - sceneRect.y()) / sceneRect.height());
 
         QPainterPath backgroundPath;
         auto bgColor = colors::light::green;
-        QPen pen(bgColor);
+        QPen pen(bgColor, 1.0 * _invScale);
 
         QBrush backgroundBrush(bgColor);
-        backgroundPath.addRoundedRect(x, y, w, h, 0, 0);
+        backgroundPath.addRect(x, y, w, h);
         painter->setPen(pen);
         painter->setBrush(Qt::NoBrush);
         painter->drawPath(backgroundPath);
@@ -97,51 +94,45 @@ void view::MiniMap::paint(QPainter *painter, const QStyleOptionGraphicsItem *opt
 QRectF view::MiniMap::boundingRect() const
 {
     //return QGraphicsObject::boundingRect();
-    return QRectF(0, 0, _size.width()*_invScale, _size.height()*_invScale);
+    return QRectF(0, 0, _size.width() * _invScale, _size.height() * _invScale);
 }
 
 void view::MiniMap::processAnchoring()
 {
     auto tr = scene()->views().first()->transform();
-    float scaleFactor = 1.0f/tr.m11();
+    float scaleFactor = 1.0f / tr.m11();
     _invScale = scaleFactor;
 
     float scaledW = getSize().width() * _invScale;
     float scaledH = getSize().height() * _invScale;
 
-    switch(getAnchor())
-    {
-    case view::MiniMap::TOP_LEFT:
-    {
+    switch (getAnchor()) {
+    case view::MiniMap::TOP_LEFT: {
         setPos(_parentViewport.topLeft() + QPointF(_gap, _gap));
         break;
     }
-    case view::MiniMap::TOP_RIGHT:
-    {
+    case view::MiniMap::TOP_RIGHT: {
         setPos(_parentViewport.topRight()
-               - QPointF(scaledW, 0) + QPointF(-_gap, _gap));
+            - QPointF(scaledW, 0) + QPointF(-_gap, _gap));
         break;
     }
-    case view::MiniMap::BOTTOM_RIGHT:
-    {
+    case view::MiniMap::BOTTOM_RIGHT: {
         setPos(_parentViewport.bottomRight()
-               - QPointF(scaledW, scaledH) + QPointF(-_gap, -_gap));
+            - QPointF(scaledW, scaledH) + QPointF(-_gap, -_gap));
         break;
     }
-    case view::MiniMap::BOTTOM_LEFT:
-    {
+    case view::MiniMap::BOTTOM_LEFT: {
         setPos(_parentViewport.bottomLeft()
-               - QPointF(0, scaledH) + QPointF(_gap, -_gap));
+            - QPointF(0, scaledH) + QPointF(_gap, -_gap));
         break;
     }
     }
     update();
 }
 
-void view::MiniMap::mousePressEvent(QGraphicsSceneMouseEvent *event)
+void view::MiniMap::mousePressEvent(QGraphicsSceneMouseEvent* event)
 {
-    if(event->button() == Qt::MouseButton::LeftButton)
-    {
+    if (event->button() == Qt::MouseButton::LeftButton) {
         _mousePressed = true;
 
         auto rect = boundingRect();
@@ -150,12 +141,14 @@ void view::MiniMap::mousePressEvent(QGraphicsSceneMouseEvent *event)
         float partialX = p.x() / rect.width();
         float partialY = p.y() / rect.height();
 
-        if(partialX < 0.0f || partialX > 1.0f) return;
-        if(partialY < 0.0f || partialY > 1.0f) return;
+        if (partialX < 0.0f || partialX > 1.0f)
+            return;
+        if (partialY < 0.0f || partialY > 1.0f)
+            return;
 
         auto srect = scene()->sceneRect();
-        float x = srect.x() + partialX*srect.width();
-        float y = srect.y() + partialY*srect.height();
+        float x = srect.x() + partialX * srect.width();
+        float y = srect.y() + partialY * srect.height();
 
         auto sgw = dynamic_cast<SceneGraphWidget*>(scene()->views().first());
         sgw->setCenterAnimated(QPointF(x, y), false);
@@ -166,21 +159,16 @@ void view::MiniMap::mousePressEvent(QGraphicsSceneMouseEvent *event)
 
 #include "view/SceneGraphWidget.h"
 
-void view::MiniMap::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+void view::MiniMap::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 {
-    if(_mousePressed)
-    {
-
+    if (_mousePressed) {
     }
 }
 
-void view::MiniMap::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+void view::MiniMap::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
 {
-    if(event->button() == Qt::MouseButton::LeftButton)
-    {
+    if (event->button() == Qt::MouseButton::LeftButton) {
         _mousePressed = false;
-
-
     }
 }
 
@@ -210,12 +198,12 @@ void view::MiniMap::setParentViewport(QRectF rect)
     processAnchoring();
 }
 
-const QSize &view::MiniMap::getSize() const
+const QSize& view::MiniMap::getSize() const
 {
     return _size;
 }
 
-void view::MiniMap::setSize(const QSize &newSize)
+void view::MiniMap::setSize(const QSize& newSize)
 {
     _size = newSize;
 }
